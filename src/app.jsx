30229ec1074bdfdcb2ce54c322493dde26691ef3
Nacon <mctangerina@gmail.com>
2024-12-26 16:49:21 -0300

    footer creation

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Header from "./components/header";
 import "../public/index.css";
 import Card from "./components/card";
+import Footer from "./components/footer";
 
 const App = () => {
-    return (<>
-
-
-        <Header />
-        <Card />
-
-    </>)
+    return (
+        <div className="min-h-screen flex flex-col">
+            <div className="flex-grow">
+                <Card />
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const Footer = () => {
+    return (
+        <footer className="w-full h-20  " >
+            <div className="w-full h-20 bg-blue-300" >
+           
+
+
+
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
